9b3a6bcef257a237ffd01fc2746b8e45ffdc9f8a
nicosis <nicosis@gmail.com>
2023-03-20 08:36:50 +0000

    solucion

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // Recuperamos TODOS los elementos HTML que cumplen con la condición class="button"
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
+buttons.forEach(addListenerToBtn);
+
+function addListenerToBtn(button) {
+    button.addEventListener('click', (obj) => {
+        body.style.backgroundColor = obj.target.id;
+    });
+};
